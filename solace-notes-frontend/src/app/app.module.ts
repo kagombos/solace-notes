ee963197d9e1c167d9a3c46fb2ec1cb0e9531443
Kamil Gombos <gremmlynn@gmail.com>
2023-10-04 09:08:12 -0500

    install material, add note component

--- solace-notes-frontend/src/app/app.module.ts
+++ solace-notes-frontend/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { NoteComponent } from './note/note.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
